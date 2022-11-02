d4a0a5221cd46c15a6d1c64b0b2843d5a0b26650
Sam Phipps <samuel.v.phipps@gmail.com>
2022-11-02 16:08:19 -0500

    added .env database info

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,8 +1,22 @@
 const pg = require('pg');
 
-let pool = new pg.Pool({
-  database: "cloud_todo"
-});
+/*
+postgresql://username:password@database_url/database_name
+
+*/ 
+// change config
+
+let pool;
+if (process.env.DATABASE_URL) {
+  pool = new pg.Pool({
+    connectionString: process.env.DATABASE_URL
+  })
+}
+else {
+  pool = new pg.Pool({
+    database: "cloud-todo",      // localhost
+  });
+}
 
 module.exports = pool;
 
